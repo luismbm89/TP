52f898833d170ff0c611edb5b510d28e266312dc
luismbm89 <luis.mbm89@gmail.com>
2017-11-27 10:37:21 -0600

    Prueba DropBox
    Debe decargar el API de DropBox mae

---
+++ slnPartes/PruebaDropBox.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Estilo/TicoParts.Master" AutoEventWireup="true" CodeBehind="PruebaDropBox.aspx.cs" Inherits="slnPartes.PruebaDropBox" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <asp:fileupload runat="server" id="flImage"></asp:fileupload><br />
+    <asp:button runat="server" text="Subir" id="btnSubir" onclick="Unnamed2_ClickAsync" />
+    <asp:label runat="server" text="" id="lblResult"></asp:label>
+</asp:Content>
---
+++ slnPartes/PruebaDropBox.aspx.cs
@@ -0,0 +1,36 @@
+﻿using Dropbox.Api;
+using Dropbox.Api.Files;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace slnPartes
+{
+    public partial class PruebaDropBox : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected async System.Threading.Tasks.Task Unnamed2_ClickAsync(object sender, EventArgs e)
+        {
+            DropboxClient dc = new DropboxClient("deBZ26uJD3AAAAAAAAAAjNTtV6CI6SWXFsWMOI9BRjKv8XSF0zgH7uOG_UCwR2Zf");
+            var file = "159386425737-IPs.txt";
+            var mem = new MemoryStream(Encoding.UTF8.GetBytes("TESTING"));
+            try
+            {
+                var updated = await dc.Files.UploadAsync(file, WriteMode.Overwrite.Instance, body: mem);
+            }
+            catch (Exception ex)
+            {
+                lblResult.Text = ex.Message;
+            }
+        }
+    }
+}
